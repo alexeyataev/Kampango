3d732318889fc77bca6b8719b08869b8f541cd4d
Tharun kumar <46321989+css118540@users.noreply.github.com>
2019-02-19 18:13:28 +0530

    Made changes to some classes and lightning components
    Made changes to some classes and lightning components

--- src/aura/AddToMembershipComp/AddToMembershipCompController.js
+++ src/aura/AddToMembershipComp/AddToMembershipCompController.js
@@ -3,29 +3,27 @@
         var action = component.get("c.addToMembershipValidation");
         action.setParams({"contId": component.get("v.recordId") });
         action.setCallback(this, function(response) {
-            var wasDismissed = $A.get("e.force:closeQuickAction");
-                wasDismissed.fire();
             var state = response.getState();  
-            if(state === "ERROR") {
-            var toastEvent = $A.get("e.force:showToast");
-            toastEvent.setParams({
-                title : 'Error Message',
-                message:'You Cant Add Membership, hence already membership exists for this contact',
-                messageTemplate: 'Mode is pester ,duration is 10sec and Message is overrriden',
-                duration:' 10000',
-                key: 'info_alt',
-                type: 'error',
-                mode: 'pester'
-            });
-            toastEvent.fire();
-            }
-              else if(state === "SUCCESS"){
+              if(state === "SUCCESS"){
+                  var wasDismissed = $A.get("e.force:closeQuickAction");
+                wasDismissed.fire();
                 var urlEvent = $A.get("e.force:navigateToURL");
                urlEvent.setParams({
                 "url": "/apex/nctETMembershipPaymentPage?id="+component.get("v.recordId")
               });
 
                  urlEvent.fire();
+            }
+               else if(state === "ERROR") {
+                var wasDismissed = $A.get("e.force:closeQuickAction");
+                wasDismissed.fire();
+            var toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                type: 'error',
+                title : 'Error Message',
+                message:'This Contact Already Exists Membership'              
+            });
+            toastEvent.fire();
             }
         });
         $A.enqueueAction(action);
