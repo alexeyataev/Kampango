07e023215136ee92a41cac7e9958345a5f2aeb3e
UjwalTheja <ujwal.theja@csscorp.com>
2019-02-21 18:49:13 +0530

    Changes updated as per the comments.
    Please verify the changes. Thanks

--- src/aura/AddToMembershipComp/AddToMembershipCompHelper.js
+++ src/aura/AddToMembershipComp/AddToMembershipCompHelper.js
@@ -1,5 +1,34 @@
 ({
-	helperMethod : function() {
-		
+	helperMethod : function(component, contId, callback) {
+		 var action = component.get("c.addToMembershipValidation");
+        action.setParams({"contId": component.get("v.recordId") });
+        action.setCallback(this, function(response) {
+            var state = response.getState();  
+              if(state === "SUCCESS"){
+                  
+                var urlEvent = $A.get("e.force:navigateToURL");
+               urlEvent.setParams({
+                "url": "/apex/nctETMembershipPaymentPage?id="+component.get("v.recordId")
+              });
+
+                 urlEvent.fire();
+            }
+               else if(state === "ERROR") {
+                var wasDismissed = $A.get("e.force:closeQuickAction");
+                wasDismissed.fire();
+            var toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                type: 'error',
+                title : 'Error Message',
+                duration : '1000',
+                message:'Membership already exists for this Contact'              
+            });
+            toastEvent.fire();
+            }
+            
+          
+        });
+        $A.enqueueAction(action);
+    
 	}
 })
--- src/aura/directpaymentlinkparent/directpaymentlinkparentController.js
+++ src/aura/directpaymentlinkparent/directpaymentlinkparentController.js
@@ -1,35 +1,5 @@
 ({
 	doInit : function(component, event,helper) {
-
-       var action = component.get("c.paymentHubCallout");
-       action.setParams({"contId": component.get("v.recordId") });
-       action.setCallback(this, function(response) {
-            var state = response.getState();
-            if(state === "SUCCESS") {
-                var wasDismissed = $A.get("e.force:closeQuickAction");
-                wasDismissed.fire();
-                var toastEvent = $A.get("e.force:showToast");
-                 toastEvent.setParams({
-                     type : 'Success',
-                 "title": "Success!",
-                 "message": "Payment link sent successfully"
-                });
-                toastEvent.fire();
-            }
-               if(state === "ERROR") {
-                var wasDismissed = $A.get("e.force:closeQuickAction");
-                wasDismissed.fire();
-                var toastEvent = $A.get("e.force:showToast");
-                 toastEvent.setParams({
-                     type : 'Error',
-                 "title": "Error!",
-                 "message": "Request Payment feature is only available for Credit/Debit card"
-                });
-                toastEvent.fire();
-            }
-        });
-        $A.enqueueAction(action);
+       helper.helperMethod(component, event.getParam("contId"));       
     }
-
-
 })
