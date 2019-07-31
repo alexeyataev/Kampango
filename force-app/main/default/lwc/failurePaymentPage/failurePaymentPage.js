5fe43c0693ea5df7dcecebd81426102f3ece6ad3
Igor Sharevich <igor.sharevich@greencouchconsulting.com>
2019-07-31 16:03:06 +0300

    Added failurePaymentsPage Lighting Web Component

---
+++ force-app/main/default/lwc/failurePaymentPage/failurePaymentPage.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+import NCT_LOGO from '@salesforce/resourceUrl/NCT_Logo';
+export default class FailurePaymentPage extends LightningElement {
+    logo = NCT_LOGO + '/nct_logo.png'
+}
