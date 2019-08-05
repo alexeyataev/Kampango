120533acc80118f2e2cacf94656dc1dbe479b485
anastasiya-alekseeva-gcc <anastasiya.alekseeva@greencouchconsulting.com>
2019-08-05 14:24:55 +0300

    W-001677
    edited component, test class

--- force-app/main/default/lwc/courseDetailsComponent/courseDetailsComponent.js
+++ force-app/main/default/lwc/courseDetailsComponent/courseDetailsComponent.js
@@ -1,5 +1,4 @@
 import { LightningElement, api, wire } from 'lwc';
-import {getFieldValue} from 'lightning/uiRecordApi';
 import { getRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import retrieveRelatedSessions from '@salesforce/apex/CourseDetailController.retrieveRelatedSessions';
@@ -9,10 +8,9 @@ const BOOKING_FIELDS = [
         'Booking__c.Course__c',
         'Booking__c.First_Name__c',
         'Booking__c.Last_Name__c',
-        'Booking__c.Course__r.Course_Reference__c',
         'Booking__c.Course__r.Start_Date__c',
         'Booking__c.Course__r.End_Date__c',
-        'Booking__c.Course__r.Course_Fee__c',
+        'Booking__c.Course__r.Fee__c',
         'Booking__c.Course__r.Sub_Type__c'
     ];
 
@@ -24,7 +22,6 @@ export default class CourseDetailsComponent extends LightningElement {
     @api bookingRecord;
     @api firstName;
     @api lastName;
-    @api courseRef;
     @api startDate;
     @api endDate;
     @api sessions;
@@ -49,15 +46,13 @@ export default class CourseDetailsComponent extends LightningElement {
             );
         } else if (data) {
             this.bookingRecord = data;
-            console.log(JSON.stringify(data));
             this.expirationDate = this.bookingRecord.fields.Reservation_Expiry_Date__c.value;
             this.firstName = this.bookingRecord.fields.First_Name__c.value;
             this.lastName = this.bookingRecord.fields.Last_Name__c.value;
-            this.courseRef = this.bookingRecord.fields.Course__r.value.fields.Course_Reference__c.value;
             this.startDate = this.bookingRecord.fields.Course__r.value.fields.Start_Date__c.value;
             this.endDate = this.bookingRecord.fields.Course__r.value.fields.End_Date__c.value;
             this.courseId = this.bookingRecord.fields.Course__c.value;
-            this.courseFee = this.bookingRecord.fields.Course__r.value.fields.Course_Fee__c.value;
+            this.courseFee = this.bookingRecord.fields.Course__r.value.fields.Fee__c.value;
             this.coursetype = this.bookingRecord.fields.Course__r.value.fields.Sub_Type__c.value;
             this.retrieveSessions(this.courseId);
         }
@@ -98,19 +93,12 @@ export default class CourseDetailsComponent extends LightningElement {
                 minute: "2-digit"
             }
         }
-        // },
-        // {
-        //     label: 'Notes',
-        //     fieldName: 'Notes__c',
-        //     type: 'Text'
-        // }
     ];   
 
     retrieveSessions(id){
         retrieveRelatedSessions({courseId: id})
         .then(data => {
             this.sessions = data;
-            console.log(JSON.stringify(data));
         })
         .catch(error => {
             let message = 'Unknown error';
