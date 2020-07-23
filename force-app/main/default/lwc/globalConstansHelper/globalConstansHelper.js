0e2a6c8701a94726b7b381a97dd15716f8e1b222
Igor Sharevich <igor.sharevich@greencouchconsulting.com>
2020-07-23 15:18:58 +0300

    Added constans to globalConstansHelper

--- force-app/main/default/lwc/globalConstansHelper/globalConstansHelper.js
+++ force-app/main/default/lwc/globalConstansHelper/globalConstansHelper.js
@@ -4,3 +4,20 @@ export const SESSION_DELIVERY_TYPE_ONLINE = 'Online';
 
 export const SESSION_STATUS_CONFIRMED = 'Confirmed';
 export const SESSION_TYPE_REUNION = 'Reunion';
+
+export const BOOKING_NAME = 'Booking';
+export const ACCOUNT_BRANCH_NAME = 'Branch';
+export const VENUE_NAME = 'Venue';
+
+export const RADIUS_DEFAULT = 100;
+
+export const HIDE_FROM_PUBLIC_ACCESS_TRUE = true;
+
+export const DISTANCE_OPTIONS = [
+    {value: '1', label: 'Within 1 mile'},
+    {value: '3', label: 'Within 3 miles'},
+    {value: '5', label: 'Within 5 miles'},
+    {value: '10', label: 'Within 10 miles'},
+    {value: '20', label: 'Within 20 miles'},
+    {value: '30', label: 'Within 30 miles'}
+]
