3baa347c5616f33f5551f7e0b73aeb3724e9687b
Igor Sharevich <igor.sharevich@greencouchconsulting.com>
2020-07-27 17:56:30 +0300

    Added global constants

--- force-app/main/default/lwc/globalConstansHelper/globalConstansHelper.js
+++ force-app/main/default/lwc/globalConstansHelper/globalConstansHelper.js
@@ -4,3 +4,13 @@ export const SESSION_DELIVERY_TYPE_ONLINE = 'Online';
 
 export const SESSION_STATUS_CONFIRMED = 'Confirmed';
 export const SESSION_TYPE_REUNION = 'Reunion';
+
+export const BOOKING_NAME = 'Booking';
+export const ACCOUNT_BRANCH_NAME = 'Branch';
+export const VENUE_NAME = 'Venue';
+
+export const RADIUS_DEFAULT = 100;
+
+export const HIDE_FROM_PUBLIC_ACCESS_TRUE = true;
+
+export const COURSE_URL_FIELD = 'courseUrl';
